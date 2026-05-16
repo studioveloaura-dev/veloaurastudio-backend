6969bb999bbf384b9cad73a4e9f3340a13903fb1
anjali <anjali@capitalnumbers.net>
2026-05-17 01:45:04 +0530

    changes cors issue

--- server.js
+++ server.js
@@ -36,6 +36,8 @@ app.use(cors({
   // origin: "http://localhost:5173", 
   origin:"https://veloaurastudio.com",
   credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 app.use(express.json()); // allows reading JSON from requests
 
